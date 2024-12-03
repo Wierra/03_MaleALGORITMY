e60611507c45d4971b23c3e2d2e5c80c1f921395
Věra Juračáková <juracakova.vera@gmail.com>
2024-12-03 16:32:21 +0100

    Vypracování úkolu 03.12.2024

--- script.js
+++ script.js
@@ -38,24 +38,38 @@ numbers.map((numbers) => {
 })
 
 //absolutní hodnota dělitélná třemi
-document.body.innerHTML += '<p><h3>Absolutní hodnota dělitélná třemi</h3></p>'
+document.body.innerHTML += '<p><h3>Absolutní hodnota dělitélná třemi:</h3></p>'
 numbers.map((numbers) => {
     if (Math.abs(numbers) % 3 === 0) {
         document.body.innerHTML += `${numbers}, `
     }
 })
 
-//číslo daleko od čísla 5
-
+//Vzdálenost čísla od čísla 5
+document.body.innerHTML += '<p><h3>Vzdálenost čísla od čísla 5:</h3></p>'
+numbers.map((numbers) => {
+    document.body.innerHTML += `${Math.abs(numbers - 5)}, `
+})
 
 //druhá mocnina vzdálenosti čísla od čísla 5
-
+document.body.innerHTML += '<p><h3>Mocnina vzdálenosti čísla od čísla 5:</h3></p>'
+numbers.map((numbers) => {
+    document.body.innerHTML += `${Math.abs((numbers - 5) ** 2)}, `
+})
 
 //počet záporných čísel
-
+document.body.innerHTML += '<p><h3>Počet záporných čísel:</h3></p>'
+let negativeNum = 0
+for (let num = 0; num < numbers.length; num += 1) {
+     if (numbers [num] < 0) {
+        negativeNum += 1
+        }
+}
+document.body.innerHTML += negativeNum
 
 //součet všech čísel v poli
-
+let sum = 0
+for (let i = 0; i < numbers.length; sum += 1)
 
 //průměr všech čísel v poli
 
